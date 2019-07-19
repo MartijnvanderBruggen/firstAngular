d1626cd98e1aa195e501d612aacf04c85a4018d0
Martijn van der Bruggen <martijnvanderbruggen@live.nl>
2019-07-19 13:23:20 +0200

    added mdb and carousel for front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ProductsComponent } from './products/products.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 
@@ -24,9 +26,12 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     HttpClientModule,
     FontAwesomeModule,
+    MDBBootstrapModule.forRoot(),
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ],
 })
 export class AppModule {
 }
